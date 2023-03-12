d8ce78dd8f53be5c365a74eed2cd21edf5e66fe1
chen.zhang <zhangchen64d@gmail.com>
2023-03-12 21:49:07 +0900

    update

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,25 +1,37 @@
-import React, { useCallback, useEffect, useState, useRef } from 'react';
+import React, {
+  useCallback,
+  useEffect,
+  useState,
+  useRef,
+  useMemo,
+} from 'react';
 
 function Slider({ items, title, subtitle, containerWidth }) {
   const { url } = items;
 
   const [sliderIdx, setSliderIdx] = useState(0);
-  const [itemCount, setItemCount] = useState(2);
+  const [countPerSlider, setCountPerSlider] = useState(2);
 
   const ref = useRef();
 
   const leftHandler = useCallback(
-    (idx) => {
-      console.log('leftHandler', idx);
-      setSliderIdx(idx);
+    (currIdx, count) => {
+      if (currIdx - count < 0) {
+        setSliderIdx(0);
+      } else {
+        setSliderIdx(currIdx - count);
+      }
     },
     [setSliderIdx]
   );
 
   const rightHandler = useCallback(
-    (idx) => {
-      console.log('rightHandler', idx);
-      setSliderIdx(idx);
+    (currIdx, count) => {
+      if (currIdx + count > items.length - count) {
+        setSliderIdx(items.length - count);
+      } else {
+        setSliderIdx(currIdx + count);
+      }
     },
     [setSliderIdx]
   );
@@ -29,17 +41,21 @@ function Slider({ items, title, subtitle, containerWidth }) {
     const wInt = parseInt(w);
 
     if (wInt <= 900) {
-      setItemCount(2);
+      setCountPerSlider(2);
       return;
     }
 
     if (wInt <= 1167) {
-      setItemCount(3);
+      setCountPerSlider(3);
       return;
     }
 
-    setItemCount(4);
+    setCountPerSlider(4);
   }, [containerWidth]);
+
+  const translateX = useMemo(() => {
+    return (100 / countPerSlider) * sliderIdx;
+  }, [sliderIdx, countPerSlider]);
 
   return (
     <div className="w-full" ref={ref}>
@@ -48,21 +64,21 @@ function Slider({ items, title, subtitle, containerWidth }) {
       <div className="w-full overflow-hidden flex">
         <div
           className="left-handler min-w-[21px] sm:min-w-[48px] transition transition-text bg-slider-handler hover:bg-slider-handler-hover mr-[6px] cursor-pointer z-10 flex justify-center items-center text-white text-xl hover:text-3xl"
-          onClick={() => leftHandler(sliderIdx - 1)}
+          onClick={() => leftHandler(sliderIdx, countPerSlider)}
         >
           <span>&#8249;</span>
         </div>
         <div
           className="slider flex flex-1 transition-transform"
-          style={{ transform: `translateX(-${100 * sliderIdx}%)` }}
+          style={{ transform: `translateX(-${translateX}%)` }}
         >
           {items.map((item, idx) => (
             <div
               key={idx}
               className=" px-[6px]"
               style={{
-                width: `${100 / itemCount}%`,
-                minWidth: `${100 / itemCount}%`,
+                width: `${100 / countPerSlider}%`,
+                minWidth: `${100 / countPerSlider}%`,
               }}
             >
               <div className="w-full h-full bg-teal-700 aspect-video flex items-center justify-center text-white text-lg">
@@ -73,7 +89,7 @@ function Slider({ items, title, subtitle, containerWidth }) {
         </div>
         <div
           className="right-handler min-w-[21px] sm:min-w-[48px] transition transition-text bg-slider-handler hover:bg-slider-handler-hover ml-[6px] cursor-pointer z-10 flex justify-center items-center text-white text-xl hover:text-3xl"
-          onClick={() => rightHandler(sliderIdx + 1)}
+          onClick={() => rightHandler(sliderIdx, countPerSlider)}
         >
           <span>&#8250;</span>
         </div>
